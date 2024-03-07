a48cba7856306543c36cc72e2ccc242b82003ac6
Henrique Sanchez <henriquealcici@gmail.com>
2024-03-07 19:38:23 -0300

    Correções

--- aula4/index.php
+++ aula4/index.php
@@ -10,26 +10,25 @@ $nome = "Henrique";
 $peso = 60;
 $altura = 1.7;
 
-$imc = round($peso / ($altura**2), 1);
-echo "Seu IMC é $imc, portanto seu resultado é ";
-switch($imc){
-    case($imc<18.5):
-        echo "Magreza";
+$imc = round($peso / ($altura ** 2), 1);
+switch ($imc) {
+    case($imc < 18.5):
+        echo "<h2>" . "Seu IMC é $imc, portanto seu resultado é Magreza" . "</h2>";
         break;
-    case($imc<25):
-        echo "Normal";
+    case($imc < 25):
+        echo "<h2>" . "Seu IMC é $imc, portanto seu resultado é Normal" . "</h2>";
         break;
-    case($imc<30):
-        echo "Sobrepeso";
+    case($imc < 30):
+        echo "<h2>" . "Seu IMC é $imc, portanto seu resultado é Sobrepeso" . "</h2>";
         break;
-    case($imc<35):
-        echo "Obesidade grau I";
+    case($imc < 35):
+        echo "<h2>" . "Seu IMC é $imc, portanto seu resultado é Obesidade grau I" . "</h2>";
         break;
-    case($imc<40):
-        echo "Obesidade grau II";
+    case($imc < 40):
+        echo "<h2>" . "Seu IMC é $imc, portanto seu resultado é Obesidade grau II" . "</h2>";
         break;
     default:
-        echo "Obesidade grau II";
+        echo "<h2>" . "Seu IMC é $imc, portanto seu resultado é Obesidade grau II" . "</h2>";
 }
 ?>
 </body>
